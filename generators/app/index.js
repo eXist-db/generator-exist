ec3d1c28a2954c346934a1e60325b54f7252d35e
Duncan Paterson <d.paterson@me.com>
2017-08-02 21:13:33 +0200

    see #5

--- generators/app/index.js
+++ generators/app/index.js
@@ -7,26 +7,77 @@ module.exports = class extends Generator {
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the gnarly ' + chalk.blue('generator-exist') + ' generator!'
+      'Welcome to the stupendous ' + chalk.blue('exist-app') + ' generator!'
     ));
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'GitHub',
-      message: 'will your app live on GitHub',
-      default: true
-    },{
-      type: 'confirm',
-      name: 'atom',
-      message: 'will you use atom for development?',
-      default: true
-    },{
+    const prompts = [
+// this needs to become subgenerators
+      {
       type: 'list',
-      choices: ['less', 'sass', 'plain'],
-      name: 'stylesheet',
-      message: 'select how your stylesheets should be generated',
+      choices: ['exist-design', 'plain-bootstrap', 'empty'],
+      name: 'design',
+      message: 'Which exist-db template would you like to use',
+      default: 'exist-design'
+    },
+    // {
+    //   type: 'confirm',
+    //   name: 'app-lib',
+    //   message: 'this would be a .xar package?',
+    //   default: true
+    // },
+
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What would you like to call your exist-db application?',
+      default: this.appname
+    },{
+      type: 'input',
+      name: 'short',
+      message: 'How should I abbreviate that?',
       default: 'None'
-    }];
+    },{
+      type: 'input',
+      name: 'uri',
+      message: 'please tell me the URI of your app',
+      default: ['http://exist-db/apps/', 'this.props.someAnswer.short']
+    },{
+      type: 'confirm',
+      name: 'collection',
+      message: 'will the application be deployed in the apps collection?',
+      default: 'true'
+    },
+    // Version number comes from elsewhere
+    // {
+    //   type: 'input',
+    //   name: 'version',
+    //   message: 'what is the version number?'
+    // },
+    {
+      type: 'list',
+      choices: ['alpha', 'beta', 'release'],
+      name: 'status',
+      message: 'what is the release status of your app',
+      default: 'None'
+    },{
+      type: 'confirm',
+      name: 'pre-install',
+      message: 'Would you like a pre-install.xql?',
+      default: 'true'
+    },{
+      type: 'confirm',
+      name: 'post-install',
+      message: 'Would you like a pre-install.xql?',
+      default: 'true'
+    },];
+
+    // missing prompts: atom, , js, css, git, gulp,
+    // initiate and commit inside user git directory
+
+    // Author, webpage and website from generator
+
+    // permissions of app
+
 
     return this.prompt(prompts).then(props => {
       // To access props later use this.props.someAnswer;
