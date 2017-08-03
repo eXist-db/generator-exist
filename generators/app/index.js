26a6757140dbc16be2abc86ceaca1c734770ade8
Duncan Paterson <d.paterson@me.com>
2017-08-03 17:40:58 +0200

    first pass with templating see #1

--- generators/app/index.js
+++ generators/app/index.js
@@ -11,63 +11,66 @@ module.exports = class extends Generator {
     ));
 
     const prompts = [
-      {
-      type: 'list',
-      choices: ['exist-design', 'plain', 'teipub', 'empty'],
-      name: 'design',
-      message: 'Which exist-db template would you like to use',
-      default: 'exist-design'
-    },
+    //   {
+    //   type: 'list',
+    //   choices: ['exist-design', 'plain', 'teipub', 'empty'],
+    //   name: 'design',
+    //   message: 'Which exist-db template would you like to use',
+    //   default: 'exist-design'
+    // },
+    // {
+    //   type: 'list',
+    //   choices: ['application', 'library']
+    //   name: 'applib',
+    //   message: 'Will this be a webapp or a XQuery library?',
+    //   default: 'application'
+    // },
     {
-      type: 'list',
-      choices: ['application', 'library']
-      name: 'applib',
-      message: 'Will this be a webapp or a XQuery library?',
-      default: 'application'
-    },{
       type: 'input',
       name: 'title',
       message: 'What would you like to call your exist-db application?',
       default: this.appname
-    },{
-      type: 'input',
-      name: 'short',
-      message: 'How should I abbreviate that?',
-      default: 'None'
-    },{
-      type: 'input',
-      name: 'uri',
-      message: 'please tell me the URI of your app',
-      default: ['http://exist-db/apps/', 'this.props.someAnswer.short']
-    },{
-      type: 'confirm',
-      name: 'collection',
-      message: 'will the application be deployed in the apps collection?',
-      default: 'true'
     },
+    // {
+    //   type: 'input',
+    //   name: 'short',
+    //   message: 'How should I abbreviate that?',
+    //   default: 'None'
+    // },{
+    //   type: 'input',
+    //   name: 'uri',
+    //   message: 'please tell me the URI of your app',
+    //   default: ['http://exist-db/apps/', 'this.props.someAnswer.short']
+    // },{
+    //   type: 'confirm',
+    //   name: 'collection',
+    //   message: 'will the application be deployed in the apps collection?',
+    //   default: 'true'
+    // },
     // Version number comes from elsewhere
     // {
     //   type: 'input',
     //   name: 'version',
     //   message: 'what is the version number?'
     // },
-    {
-      type: 'list',
-      choices: ['alpha', 'beta', 'stable', ''],
-      name: 'status',
-      message: 'what is the release status of your app',
-      default: ''
-    },{
-      type: 'confirm',
-      name: 'pre',
-      message: 'Would you like a pre-install.xql?',
-      default: 'true'
-    },{
-      type: 'confirm',
-      name: 'post',
-      message: 'Would you like a post-install.xql?',
-      default: 'false'
-    },];
+    // {
+    //   type: 'list',
+    //   choices: ['alpha', 'beta', 'stable', ''],
+    //   name: 'status',
+    //   message: 'what is the release status of your app',
+    //   default: ''
+    // },{
+    //   type: 'confirm',
+    //   name: 'pre',
+    //   message: 'Would you like a pre-install.xql?',
+    //   default: 'true'
+    // },{
+    //   type: 'confirm',
+    //   name: 'post',
+    //   message: 'Would you like a post-install.xql?',
+    //   default: 'false'
+    // }
+  ];
 
     // missing prompts: atom, ,js, css, git, gulp, funcdoc,
     // initiate and commit inside user git directory
@@ -96,6 +99,28 @@ module.exports = class extends Generator {
       this.templatePath('exist-design/collection.xconf'),
       this.destinationPath('collection.xconf')
     );
+
+    // The basics: build, expath-pkg, and repo.
+    this.fs.copyTpl(
+      this.templatePath('build.xml'),
+      this.destinationPath('build.xml'),
+      {'title': this.props.title}
+    );
+    this.fs.copyTpl(
+      this.templatePath('repo.xml'),
+      this.destinationPath('repo.xml'),
+      {'collection': this.props.collection}
+    );
+    this.fs.copyTpl(
+      this.templatePath('expath-pkg.xml'),
+      this.destinationPath('expath-pkg.xml'),
+      {'short': this.props.short}
+    );
+    this.fs.copyTpl(
+      this.templatePath('exist-design/templates/page.html'),
+      this.destinationPath('templates/page.html'),
+      {'title': this.props.title}
+    )
   }
 
   install() {
