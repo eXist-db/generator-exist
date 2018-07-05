18072caad7e9f8e754a15425f5176fa971e98282
Duncan Paterson <d.paterson@me.com>
2018-07-05 17:26:18 +0200

    add library test

--- test/app.js
+++ test/app.js
@@ -3,30 +3,35 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-exist:app', function () {
-  before('create app dir', function () {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({
-        title: "foo",
-        author: "tester",
-        email: "te@st.er"
-      })
-      .then(function(){true});
+describe('exist scaffold app', function() {
+  describe('eXide style app', function() {
+    it('create app dir', function(done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
+        .withPrompts({
+          title: "foo",
+          author: "tester",
+          email: "te@st.er"
+        })
+        .on('end', function(){
+          describe('basic app has', function() {
+            it('default files', function() {
+              assert.file(['repo.xml', 'modules/app.xql'])
+            });
+
+            it('with proper names inside', function() {
+              assert.fileContent('repo.xml', /<target>foo<\/target>/)
+            });
+
+          });
+          done();
+        })
+      });
   });
 
-  it('has default files', function() {
-      assert.file(['repo.xml', 'README.md', 'pre-install.xql', 'post-install.xql', 'modules/app.xql']);
-    }),
 
-    it('does not have this', function() {
-      assert.noFile(['frrrrepo.xml']);
-    });
 
-  it('repo has correct name', function() {
-    assert.fileContent('repo.xml', /<target>foo<\/target>/);
-  });
 
-  // after('cleanup', function() {
-  // rm -rf .git
-  // });
 });
+// it('does not have this', function() {
+//   assert.noFile(['frrrrepo.xml']);
+// });
