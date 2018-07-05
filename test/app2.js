34d385b476e874d71042fbfe6add648c992bee25
Duncan Paterson <d.paterson@me.com>
2018-07-05 18:04:55 +0200

    cleanup

--- test/app2.js
+++ test/app2.js
@@ -2,25 +2,33 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+var fs = require('fs-extra');
 
 describe('library package', function() {
-      describe('library', function() {
-        it('create lib pkg', function(done) {
-          helpers.run(path.join(__dirname, '../generators/app'))
-            .withPrompts({
-              title: "foo",
-              author: "tester",
-              email: "te@st.er",
-              apptype: 'library',
-            })
-            .on('end', function() {
-              describe('library has', function() {
-                it('default files', function() {
-                  assert.file(['repo.xml', 'content/.gitkeep'])
-                });
-              });
-              done();
-            })
-        });
-      });
+before(function() {
+  return helpers.run(path.join(__dirname, '../generators/app'))
+    .withPrompts({
+      title: "foo",
+      author: "tester",
+      email: "te@st.er",
+      apptype: ['empty', 'library'],
+      pre: false,
+      post: false
+    })
+    .then(function() {
+      return true;
     });
+});
+
+
+  describe('library has', function() {
+    it('recommended files', function() {
+      assert.file(['repo.xml', 'content/.gitkeep'])
+    });
+  });
+
+
+after('teardown', function() {
+  fs.emptydirSync(process.cwd())
+});
+});
