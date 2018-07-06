89f206967bafcb1f382f3dec5f88fecbd6d0199b
Duncan Paterson <d.paterson@me.com>
2018-07-06 15:25:34 +0200

    add remaing app tests as pending

---
+++ test/app-polymer-element.js
@@ -0,0 +1,35 @@
+'use strict'
+var path = require('path')
+var assert = require('yeoman-assert')
+var helpers = require('yeoman-test')
+var fs = require('fs-extra')
+
+describe.skip('polymer element', function () {
+  before(function () {
+    this.timeout(3000)
+    return helpers.run(path.join(__dirname, '../generators/app'))
+      .withPrompts({
+        title: 'foo',
+        author: 'tester',
+        email: 'te@st.er',
+        apptype: ['polymer', 'application']
+      })
+      .then(function () {
+        return assert(true)
+      })
+  })
+
+  describe('plain app has', function () {
+    it('recommended files', function () {
+      assert.file(['repo.xml', 'modules/app.xql'])
+    })
+
+    it('with proper names inside', function () {
+      assert.fileContent('repo.xml', /<target>foo<\/target>/)
+    })
+  })
+
+  after('teardown', function () {
+    fs.emptydirSync(process.cwd())
+  })
+})
